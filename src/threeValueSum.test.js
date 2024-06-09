0efad4c1fb677fc2f231a96770c68efaae1c6521
SauravNaruka <saurav.singh.n@gmail.com>
2024-06-09 18:49:13 +0530

    Update sum of three numbers

--- src/threeValueSum.test.js
+++
@@ -1,7 +0,0 @@
-import { findSumOfThree } from "./threeValueSum";
-
-describe("K Crystal Problem", () => {
-  test("if result is correct", () => {
-    expect(findSumOfThree([3, 7, 1, 2, 8, 4, 5], 10)).toBe(true);
-  });
-});
